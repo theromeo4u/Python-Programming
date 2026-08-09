61ef64d22bf87aa9073ca1d4e6f27d85f33050b0
Subodhkumar <snp42u@gmail.com>
2026-08-09 15:45:44 +0530

    Refactor Rock-Paper-Scissors game to include score tracking and loop for replay

--- Rock_paper_scissors_project/main.py
+++ Rock_paper_scissors_project/main.py
@@ -1,29 +1,96 @@
+# import random
+# '''
+# -1 for Rock
+# 1 for scissors
+# 0 for paper
+# '''
+# user_score = 0
+# computer_score = 0
+
+
+# computer = random.choice([-1,1,0])
+# youstr = input("Enter your choice (r = Rock, p = Paper, s = Scissors): ").lower()
+# yourDict = {'r': -1, 's': 1, 'p': 0}
+# reverseDict = {-1: "Rock", 1: "Scissors", 0: "Paper"}
+
+# if youstr not in yourDict:
+#     print("Invalid Input choose (r = Rock, p = Paper, s = Scissors): ") 
+#     exit()
+    
+# you = yourDict[youstr]
+
+# print(f"You choose '{reverseDict[you]}' and Computer choose '{reverseDict[computer]}'")
+
+
+# if computer == you:
+#     print("It's a Draw!")
+
+# elif computer == -1 and you == 0:
+#     print("You Win!")      # Paper beats Rock
+
+# elif computer == 0 and you == 1:
+#     print("You Win!")      # Scissors beats Paper
+
+# elif computer == 1 and you == -1:
+#     print("You Win!")      # Rock beats Scissors
+
+# else:
+#     print("You Lose!")
+
+
 import random
-'''
--1 for Rock
-1 for scissors
-0 for paper
-'''
-computer = random.choice([-1,1,0])
-youstr = input("Enter your choice (r = Rock, p = Paper, s = Scissors): ").lower()
-yourDict = {'r': -1, 's': 1, 'p': 0}
-reverseDict = {-1: "Rock", 1: "Scissors", 0: "Paper"}
 
-you = yourDict[youstr]
+userScore = 0
+computerScore = 0
 
-print(f"You choose '{reverseDict[you]}' and Computer choose '{reverseDict[computer]}'")
+while True:
+    '''
+    -1 for Rock
+    1 for scissors
+    0 for paper
+    '''
+    computer = random.choice([-1,1,0])
+    youstr = input("Enter your choice (r = Rock, p = Paper, s = Scissors): ").lower()
+    yourDict = {'r': -1, 's': 1, 'p': 0}
+    reverseDict = {-1: "Rock", 1: "Scissors", 0: "Paper"}
 
-if computer == you:
-    print("It's a Draw!")
+    if youstr not in yourDict:
+        print("Invalid Input choose (r = Rock, p = Paper, s = Scissors): ")
+        continue
 
-elif computer == -1 and you == 0:
-    print("You Win!")      # Paper beats Rock
+    you = yourDict[youstr]
 
-elif computer == 0 and you == 1:
-    print("You Win!")      # Scissors beats Paper
+    print(f"You choose '{reverseDict[you]}' and Computer choose '{reverseDict[computer]}'")
 
-elif computer == 1 and you == -1:
-    print("You Win!")      # Rock beats Scissors
+    if computer == you:
+        print("It's a Draw!")
 
-else:
-    print("You Lose!")
+    elif computer == -1 and you == 0:
+        print("You Win!")
+        userScore += 1
+
+    elif computer == 0 and you == 1:
+        print("You Win!")
+        userScore += 1
+
+    elif computer == 1 and you == -1:
+        print("You Win!")
+        userScore += 1
+
+    else:
+        print("You Lose!")
+        computerScore += 1
+
+    print(f"\nScore")
+    print(f"You : {userScore}")
+    print(f"Computer : {computerScore}")
+
+    playAgain = input("\nDo you want to play again? (y/n): ").lower()
+
+    if playAgain != 'y':
+        break
+
+print("\nGame Over!")
+print(f"Final Score")
+print(f"You : {userScore}")
+print(f"Computer : {computerScore}")
