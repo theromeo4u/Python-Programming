21757bae1abffdc7ff9ebe1ce5818269ba785b3d
Subodhkumar <snp42u@gmail.com>
2026-08-10 10:34:58 +0530

    Generate multiplication tables from 2 to 20 and save to individual text files

--- Day6/table.py
+++ Day6/table.py
@@ -2,7 +2,7 @@ def generateTable(n):
     table = ""
     for i in range(1,11):
         table += f"{n} X {i} = {n*i}\n"
-        with open(f"tables/table_{n}", "w") as f:
+        with open(f"tables/table_{n}.txt", "w") as f:
             f.write(table)
 
 
