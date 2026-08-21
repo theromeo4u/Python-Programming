18fe3ff13819bc7100ec58b39b58d245546c32c3
Subodh Kumar <snp42u@gmail.com>
2026-08-21 22:51:50 +0530

    Add Employee class with attributes and method to display information

---
+++ Day7/class_obj.py
@@ -0,0 +1,8 @@
+class Employee:
+    lang = "Python"
+    salary = 80000
+    def getInfi(self):
+        print(f"The language is {self.lang} and the salary is {self.salary}")
+        
+subodh = Employee()
+print(subodh.lang, subodh.salary)
