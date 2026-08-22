a2128fdb35ade7585eb744a16ffc9ebcded2d761
Subodh Kumar <snp42u@gmail.com>
2026-08-22 18:54:02 +0530

    Update getInfo method to include company name in salary display

--- Day7/Programmers.py
+++ Day7/Programmers.py
@@ -1,12 +1,12 @@
 class Programmers:
-    
+    comapany = "Microsoft"
     def __init__(self,name, salary):
         self.name = name
         self.salary = salary
         print("Creating object.......")
         
     def getInfo(self):
-        print(f"The salary of {self.name} is {self.salary}")
+        print(f"The salary of {self.name} is {self.salary} working at {self.comapany}")
         
 p1 = Programmers("Subodh", 350000)
 p1.getInfo()
