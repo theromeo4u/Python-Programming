75b63bcd3bb6d8acaca390409ae964726ea1e5c9
Subodh Kumar <snp42u@gmail.com>
2026-08-22 19:06:42 +0530

    Add squareroot method to Calculator class and update input prompt

--- Day7/math.py
+++ Day7/math.py
@@ -9,9 +9,12 @@ class Calculator:
     def cube(self):
         ans = self.radius * self.radius * self.radius
         print(f"The Cube of {self.radius} is {ans}")
-            
-num = int(input("Enter a number for Square and Cube: "))
+    def squareroot(self):
+            ans = self.radius ** 1/2
+            print(f"The squareroot of {self.radius} is {ans}")        
+num = int(input("Enter a number for Square, Squareroot and Cube: "))
 
 c = Calculator(num)
 c.square()
 c.cube()
+c.squareroot()
