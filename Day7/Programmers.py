5046c1ec01e2cb29e4a352c2f4d355b6d902a849
Subodh Kumar <snp42u@gmail.com>
2026-08-22 18:51:45 +0530

    Add Programmers class with initialization and method to display salary information

---
+++ Day7/Programmers.py
@@ -0,0 +1,14 @@
+class Programmers:
+    
+    def __init__(self,name, salary):
+        self.name = name
+        self.salary = salary
+        print("Creating object.......")
+        
+    def getInfo(self):
+        print(f"The salary of {self.name} is {self.salary}")
+        
+p1 = Programmers("Subodh", 350000)
+p1.getInfo()
+p2 = Programmers("Romeo", 100000)
+p2.getInfo()
