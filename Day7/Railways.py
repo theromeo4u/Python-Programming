21863ed101fac8b353b3877b80b3028bb461f298
Subodh Kumar <snp42u@gmail.com>
2026-08-23 07:51:21 +0530

    Refactor Rail class methods for improved clarity and consistency in booking and fare calculation

--- Day7/Railways.py
+++ Day7/Railways.py
@@ -2,15 +2,23 @@ import random
 
 
 class Rail:
-    def book(self):
-        print(f"Ticket is booked on {self.trainNo} from {self.From} to {self.To}")
-    
-    def getFare(self,trainNo,From,To):
+    def __init__(self,trainNo):
         self.trainNo = trainNo
+    def book(self,From,To):
         self.From = From
         self.To = To
-        print(f"Ticket Fare of train no: {self.trainNo} from {self.From} to {self.To} is {random.randint(100,999)}")
+        print(f"Ticket is booked on train no:{self.trainNo} from {self.From} to {self.To}")
+    
+    def getFare(self,From,To):
+        self.From = From
+        self.To = To
+        print(f"Ticket Fare of train no: {self.trainNo} from {From} to {To} is {random.randint(100,999)}")
 
-    def getStatus(self, trainNo):
-        print(f"Status of train no: {trainNo} is running on time")
-        
+    def getStatus(self,):
+        print(f"Status of train no: {self.trainNo} is running on time")
+    
+t = Rail(11217)
+t.book("Mumbai", "Amritsar")
+t.getFare("Mumbai", "Amritsar")
+t.getStatus()
+    
