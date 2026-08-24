1f8232f54c303838eb43a2ef1b078128cdfab6a4
Subodh Kumar <snp42u@gmail.com>
2026-08-24 16:24:04 +0530

    Add Employee and Programmer classes with methods for displaying information and language proficiency

--- Day9/inheritance.py
+++ Day9/inheritance.py
@@ -1,2 +1,28 @@
 class Employee:
-    pass
+    company = "Meta"
+    def __init__(self,name , salary,language):
+        self.name = name
+        self.salary = salary
+        self.language = language
+    def getInfo(self):
+        print(f"The name of the company is {self.company} and salary is {self.salary}")
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+
+class Programmer:
+    company = "Meta"
+    def __init__(self,name , salary,language):
+            self.name = name
+            self.salary = salary
+            self.language = language
+    def getInfo(self):
+        print(f"The name of the company is {self.company} and salary is {self.salary}")
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")    
+        
+a = Employee("Romeo",60000,"Python")
+b = Programmer("Subodh", 50000,"Java")
+a.getInfo()
+a.showLanguage()
+b.getInfo()        
+b.showLanguage()
