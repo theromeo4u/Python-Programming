f9f510fc0013ef5a093aa8df3e39b569b710b4fb
Subodhkumar <111661624+theromeo4u@users.noreply.github.com>
2026-08-31 22:48:02 +0530

    Update friends list with new names and fix sort
    Corrected the sort method call to sort the list properly.

--- Day2/list.py
+++ Day2/list.py
@@ -1,7 +1,7 @@
 friends = ["Subodh", "Rahul",23,65,"Akash","Suraj"]
 friends[2]="Romeo"
 
-friends.append("Ishwar")
+friends.append("Ishwar")  #add at the end of list
 friends.insert(3,"Ashok")
 friends.sort
 print(friends)
