48d6c7a836e6d90b640c151b14a987f7aed065d0
Subodhkumar <111661624+theromeo4u@users.noreply.github.com>
2026-08-31 22:52:50 +0530

    Uncomment username length check in count.py

--- Day3/count.py
+++ Day3/count.py
@@ -5,7 +5,7 @@ userName = input("Enter user name: ")
 #     print("Username contains more than 10 character")
 # else:
 #     print("Username contains less than 10 character")
-
+# typeof(length)
 
 if(len(userName)>=10):
     print("Username contains more than 10 character")
