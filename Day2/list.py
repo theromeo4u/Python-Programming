049d722e90bfccfac046b441916876faee061b1d
Subodhkumar <111661624+theromeo4u@users.noreply.github.com>
2026-08-31 22:48:53 +0530

    Update friends list and fix sorting method
    Fixed sorting method and updated list elements.

--- Day2/list.py
+++ Day2/list.py
@@ -2,6 +2,6 @@ friends = ["Subodh", "Rahul",23,65,"Akash","Suraj"]
 friends[2]="Romeo"
 
 friends.append("Ishwar")  #add at the end of list
-friends.insert(3,"Ashok")
+friends.insert(3,"Ashok") #insert to the mentioned location
 friends.sort
 print(friends)
